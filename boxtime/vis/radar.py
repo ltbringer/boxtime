e53018b07754078bfb546f95d05740c241d3a9db
ltbringer <amresh.venugopal@gmail.com>
2023-12-29 08:11:56 +0000

    fix: radar chart uses data[tag] for plot.

--- boxtime/vis/radar.py
+++ boxtime/vis/radar.py
@@ -1,13 +1,12 @@
 from typing import Dict, List
 
-import pandas as pd
 import numpy as np
-from seaborn import violinplot, color_palette
 import matplotlib.pyplot as plt
 
 from boxtime.client.calendar import Event
 from boxtime.vis.aggregate import group_by, AggField
-from boxtime.vis.colors import color_map, Color
+from boxtime.vis.colors import Color
+from boxtime.vis.utils import save_plot
 
 
 def plot_radar(
@@ -27,18 +26,19 @@ def plot_radar(
             continue
         data[tag] = sum([e.duration for e in t_events])
 
-    normalized_values = list(data.values())
+    values = list(data.values())
 
-    theta = np.arange(len(normalized_values) + 1) / len(normalized_values) * 2 * np.pi
-    values = np.array(normalized_values + [normalized_values[0]])
+    theta = np.arange(len(values) + 1) / len(values) * 2 * np.pi
+    values_ = np.array(values + [values[0]])
 
     fig = plt.figure(figsize=(20, 10), dpi=300)
     ax = fig.add_subplot(111, polar=True)
-    ax.plot(theta, values, color="C2", marker=".", label="Week N")
-    plt.xticks(
-        theta[:-1], [k.title() for k in reverese_tags.keys()], color="#000", size=8
-    )
-    ax.fill(theta, values, "green", alpha=0.1)
 
-    plt.title("Title")
-    plt.show()
+    ax.plot(theta, values_, color="C2", marker=".", label="Week N")
+    plt.xticks(theta[:-1], [k.title() for k in data.keys()], color="#000", size=8)
+
+    ax.fill(theta, values_, "green", alpha=0.1)
+    plt.title("(Un)Balanced time investment.")
+
+    if save_key:
+        save_plot(save_key, "radar.png")
