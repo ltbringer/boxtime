2c23b115925bc3cc159d5df8fe66ef3077c9e02e
ltbringer <amresh.venugopal@gmail.com>
2023-12-29 03:46:30 +0000

    add: db modules.

--- boxtime/vis/__init__.py
+++ boxtime/vis/__init__.py
@@ -7,4 +7,3 @@ home = str(Path.home())
 fp = font_manager.FontProperties(fname=Path.home() / ".fonts/Roboto-Light.ttf")
 font_manager.findfont(fp)
 plt.rcParams["font.family"] = fp.get_name()
-# plt.rcParams["font.weight"] = "light"
